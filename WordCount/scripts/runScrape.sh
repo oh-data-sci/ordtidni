b0bc43060cde48822df7f7a7b258303dbc7214db
Borkur Steingrimsson <borkur.steingrimsson@rittmanmead.com>
2019-09-21 20:57:27 +0100

    working, but simple example to convert XML to parquet

--- WordCount/scripts/runScrape.sh
+++ WordCount/scripts/runScrape.sh
@@ -1,19 +1,39 @@
-:
+#!/usr/bin/env bash
 
-export SPARK_JAVA_OPTS="-Xmx16g" 
+set -x
 
-JARS=/Users/borkur/Downloads/Gigaword/jar_files/hadoop-aws-3.2.0.jar,/Users/borkur/Downloads/Gigaword/jar_files/aws-java-sdk-bundle-1.11.375.jar,/Users/borkur/Documents/GIT/databricks/spark-xml/target/scala-2.11/spark-xml_2.11-0.5.0.jar
+# H
+#PATH=/Users/borkur/Downloads/spark-2.4.0-bin-custom-spark-2.4.0-hadoop-3.2/bin:$PATH
 
---conf "spark.driver.extraClassPath=/Users/borkur/Downloads/Gigaword/jar_files/aws-java-sdk-bundle-1.11.375.jar" \
---conf "spark.driver.extraClassPath=/Users/borkur/Downloads/Gigaword/jar_files/hadoop-aws-3.2.0.jar" \
+export SPARK_JAVA_OPTS="-Xmx16g"
+#Where I've placed the various JAR files to get all of this to work :/
+JR=/Users/borkur/Downloads/Gigaword/jar_files
 
+# This files gets created if I manually run `sbt package`
+SCRAPE_JAR=/Users/borkur/Documents/GIT/oh-data-sci/ordtidni/WordCount/target/scala-2.11/ordtidnixml_2.11-0.1.jar
+# this file gets built with IntelliJ
+# SCRAPE_JAR=$JR/OrdtidniXML.jar
+
+#JARS=$JR/hadoop-aws-3.2.0.jar,$JR/aws-java-sdk-s3-1.11.595.jar,$JR/spark-xml_2.11-0.5.0.jar,$JR/hadoop-lzo.jar
+#JARS=$JR/hadoop-aws-3.2.0.jar,$JR/aws-sdk-java-s3-2.7.9-SNAPSHOT.jar,$JR/spark-xml_2.11-0.5.0.jar,$JR/hadoop-lzo.jar,$JR/OrdtidniXML.jar,$JR/hadoop-common-3.2.0.jar
+JARS=$JR/hadoop-aws-3.1.2.jar,$JR/spark-xml_2.11-0.5.0.jar,$JR/hadoop-lzo.jar,$JR/hadoop-common-3.1.2.jar,$JR/aws-java-sdk-bundle-1.11.271.jar,$JR/OrdtidniXML.jar
+#JARS=$JR/aws-java-sdk-bundle-1.11.271.jar,$JR/spark-xml_2.11-0.5.0.jar,$JR/hadoop-aws-3.1.2.jar
+#JARS=$JR/OrdtidniXML.jar
+
+#--conf spark.driver.extraClassPath=$JR/aws-java-sdk-s3-1.11.595.jar \
+#--conf spark.driver.extraClassPath=$JR/hadoop-aws-3.1.2.jar \
+#--conf spark.driver.extraClassPath=$JR/spark-xml_2.11-0.5.0.jar \
+#--conf spark.jars=$JARS \
+#--jars $JARS \
+
+#--packages com.amazonaws:aws-java-sdk:1.7.4,org.apache.hadoop:hadoop-aws:2.7.3 \
+#--packages com.amazonaws:aws-java-sdk-s3:1.11.594,org.apache.hadoop:hadoop-aws:3.1.2,com.databricks:spark-xml_2.11:0.5.0 \
+#--conf spark.hadoop.fs.s3.impl=org.apache.hadoop.fs.s3a.S3AFileSystem \
 spark-submit \
---packages com.amazonaws:aws-java-sdk:1.7.4,org.apache.hadoop:hadoop-aws:2.7.3 \
 --conf spark.hadoop.fs.s3.awsAccessKeyId=C$AWS_ACCESS_KEY_ID \
 --conf spark.hadoop.fs.s3.awsSecretAccessKey=$AWS_SECRET_ACCESS_KEY \
 --conf spark.hadoop.fs.s3.impl=org.apache.hadoop.fs.s3a.S3AFileSystem \
---conf spark.driver.extraClassPath=/Users/borkur/Downloads/Gigaword/jar_files/aws-java-sdk-bundle-1.11.375.jar \
---conf spark.driver.extraClassPath=/Users/borkur/Downloads/Gigaword/jar_files/hadoop-aws-3.2.0.jar \
---master local --executor-memory 12G --class XMLScraper --name XMLScraper_App \
+--conf spark.jars=$JARS \
 --jars $JARS \
-/Users/borkur/Documents/GIT/oh-data-sci/ordtidni/WordCount/target/scala-2.11/scalaspark_2.11-0.1.jar
+--master local[4] --executor-memory 2G --class XMLScraper --name XMLScraper_App \
+$SCRAPE_JAR $*
--- WordCount/src/main/scala/XMLScraper.scala
+++ WordCount/src/main/scala/XMLScraper.scala
@@ -2,18 +2,23 @@
 import org.apache.spark.sql._
 import com.databricks.spark.xml._
 import org.apache.hadoop.fs._
-//import com.amazonaws.
+import org.apache.spark.sql.streaming.{OutputMode, Trigger}
+import org.apache.spark.sql.types.{StringType, StructField, StructType}
+import org.apache.hadoop.io.{LongWritable, Text}
+import com.databricks.spark.xml.XmlInputFormat
+
+
 
 object XMLScraper {
   def main(args: Array[String]) {
 
     val n = args.length
 
-    val usage = "Usage: XMLScraper --dataset Dataset  [--source Source String] [--year Year num] " +
-      //+"[--month Month num] " +
+    val usage = "Usage: XMLScraper  [--source Source String] [--year Year num] " +
+      "[--month Month num] " +
       "e.g. MIM mbl 2015 10"
 
-    if(n < 6)
+    if(n < 4)
       throw new Exception ("Must provide dataset, source and year to read from: "+ usage)
 
     val arglist = args.toList
@@ -39,59 +44,100 @@ object XMLScraper {
       }
     }
 
-    var l_options = nextOption(Map(),arglist)
+    val l_options = nextOption(Map(),arglist)
     println(l_options)
-    var l_ds = l_options('dataset)
-    println(l_ds)
-    var l_source = l_options('source)
+   // var l_ds = l_options('dataset)
+    //println(l_ds)
+    val l_source = l_options('source)
     println(l_source)
     var l_year = l_options('year)
     println(l_year)
-//    var Any l_month="*"
-//    try {
-//      l_month = l_options('month)
-//    }
-//    println(l_month)
+    var l_month = l_options('month)
+    println(l_month)
     //throw new Exception ("going home  "+ arglist.toStream)
 
+    if(l_year == "x")
+      l_year="*"
 
-    //val sc = SparkSession.builder.master("local").appName("XMLScraper").getOrCreate()
+    if(l_month == "x")
+      l_month="*"
+
+
+//    val sc = SparkSession.builder.master("local").appName("XMLScraper").getOrCreate()
     val sc = SparkSession.builder.getOrCreate()
-    var l_inputString = "s3a://borkur-gigaword/XML/" + l_ds + "/" +  l_source + "/" +l_year
 
-    //sc.hadoopConfiguration.set("fs.s3n.awsAccessKeyId", accessKeyId)
-    //sc.hadoopConfiguration.set("fs.s3n.awsSecretAccessKey", secretAccessKey)
+
+
+    //val xml = sc.fileStream[LongWritable,Text,XmlInputFormat](monitoredDirectory,true,false)
+
+//    var l_inputString  = "s3a://ordtidni/XML/" + l_ds + "/" +  l_source + "/" +l_year
+//    var l_outputString = "s3a://ordtidni/output/" + l_ds + "/" +  l_source + "/" +l_year
+
+    val l_inputString  = "s3a://ordtidni/XML/" +  l_source + "/" +l_year + "/" +l_month
+//    val l_outputString = "s3a://ordtidni/output/" + l_source + "/" //+l_year + "/" +l_month
+//    val l_inputString = "/Users/borkur/Downloads/"
+
+//    var l_inputString = "/Users/borkur/Downloads/Arnastofnun/" + l_ds + "/" +  l_source + "/" +l_year
+    var l_outputString = "/tmp/" + "/" +  l_source + "/" +l_year
+
+    println("Input:  "+l_inputString)
+    println("Output: "+l_outputString)
 
     // TODO This is perhaps better done with Spark Streaming ?
     // TODO handle dataset, source, year and month. Still think Spark Streaming is what we need
-    val l_dataset = args
+
     val giga_schema = sc.read
       .format("com.databricks.spark.xml")
       .option("rowTag", "s") // Only reading in 'S' = 'Sentence'
-      .xml("/tmp/schema.xml").schema //Get the schema from a single XML example to use for the bulk
+      .xml("/tmp/schema.xml")
+      .schema //Get the schema from a single XML example on HDFS to use for the bulk
 
     val df = sc.read
       .format("com.databricks.spark.xml")
       .option("rowTag", "s") // Only reading in 'S' = 'Sentence'
-      .schema(giga_schema) //This saved 10 minutes over 43.000 files. fro 10m to 0.4 s for this step. (morgunbladid/2015)
-      .xml(l_inputString+"/*/*.xml") // Given dataset, source and year: add all months and all XML files
+      .schema(giga_schema) //This saved 10 minutes over 43.000 files. fro 10m to 0.4 s for this step. (morgunbladid/2015). Using one XML file as the schema to avoid schema discovery each time
+      .xml(l_inputString+"/*.xml") // Given dataset, source and year: add all months and all XML files
       .withColumn("Word",functions.explode(functions.col("w"))) // Flatten out
-      .select("Word._lemma","Word._type","Word._VALUE").toDF("Lemma","POS","Word") // Just the columns we want.
-      //.repartition(600,functions.col("POS")) // Not convinced this is doing what I think it does. at least not getting one partition per POS
+      .select("Word._lemma","Word._type","Word._VALUE")
+      .toDF("Lemma","POS","Word") // Just the columns we want.
 
-//    df.write
-//        .format("com.databricks.spark.csv")
-//        .option("header", "false")
-//        .mode("overwrite")
-//        .save("/tmp/scrape/") // save to HDFS
+//      val fileStreamDf = sc.readStream
+//        .format("com.databricks.spark.xml")
+//          .format("xml")
+//        .option("rowTag", "s") // Only reading in 'S' = 'Sentence'
+//        .schema(giga_schema) //This saved 10 minutes over 43.000 files. fro 10m to 0.4 s for this step. (morgunbladid/2015)
+//        .load(l_inputString+"/*/*.xml") // Given dataset, source and year: add all months and all XML files
+//        .withColumn("Word",functions.explode(functions.col("w"))) // Flatten out
+//        .select("Word._lemma","Word._type","Word._VALUE")
+//        .toDF("Lemma","POS","Word") // Just the columns we want.
 
-    df.coalesce(1)
+//
+//    df.coalesce(1)
+    ////      .write
+    ////      .format("com.databricks.spark.csv")
+    ////      .option("header", "false")
+    //////      .mode("overwrite")
+    ////      .mode("append")
+    ////      .save(l_outputString) // save to S3
+
+
+    df.coalesce(20)
       .write
-      .format("com.databricks.spark.csv")
-      .option("header", "false")
-      //.mode("overwrite")
+      .format("parquet")
+      //.option("header", "false")
+      //      .mode("overwrite")
       .mode("append")
-      .save("s3a://borkur-gigaword/output") // save to S3
+      .save(l_outputString) // save to S3
+
+
+//    val query = fileStreamDf.writeStream
+//      .format("com.databricks.spark.csv")
+//      .option("header", "false")
+//      .outputMode(OutputMode.Append())
+//      .option("path",l_outputString) // save to S3
+//      .start()
+//
+//    query.awaitTermination()
 
 
   }
